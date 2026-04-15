8786713ec2393fc04bc6d369f2065f05f12e0878
Haiyan Wang <hw301@duke.edu>
2026-04-15 05:55:58 -0400

    created general purpose univariate representation architecture

--- src/experiments/synthetic_univariate/univariate_model.py
+++ src/experiments/synthetic_univariate/univariate_model.py
@@ -41,13 +41,55 @@ class PPGModel(nn.Module):
             ),
             ResidualBlock(
                 layer_configs = [
+                    {'in_channels' : 16, 'out_channels' : 16, 'kernel_size' : 5},
+                    {'in_channels' : 16, 'out_channels' : 16, 'kernel_size' : 5}
+                ]
+            ),
+            ResidualBlock(
+                layer_configs = [
                     {'in_channels' : 16, 'out_channels' : 32, 'kernel_size' : 5},
                     {'in_channels' : 32, 'out_channels' : 32, 'kernel_size' : 5}
                 ]
             ),
             ResidualBlock(
                 layer_configs = [
+                    {'in_channels' : 32, 'out_channels' : 32, 'kernel_size' : 5},
+                    {'in_channels' : 32, 'out_channels' : 32, 'kernel_size' : 5}
+                ]
+            )
+        )
+
+        self.prototype_layer_1 = PrototypeLayer(
+            n_prototypes = 25,
+            prototype_len = 40,
+            prototype_channels = 32,
+        )
+
+        self.temporal_mixing_layer_1 = nn.Sequential(
+            ResidualBlock(
+                layer_configs = [
+                    {'in_channels' : 25, 'out_channels' : 50, 'kernel_size' : 3},
+                    {'in_channels' : 50, 'out_channels' : 50, 'kernel_size' : 3}
+                ]
+            ),
+            ResidualBlock(
+                layer_configs = [
+                    {'in_channels' : 50, 'out_channels' : 50, 'kernel_size' : 5},
+                    {'in_channels' : 100, 'out_channels' : 100, 'kernel_size' : 5}
+                ]
+            )
+        )
+
+        self.encoder_layer_2 = nn.Sequential(
+            ResidualBlock(
+                layer_configs = [
                     {'in_channels' : 32, 'out_channels' : 64, 'kernel_size' : 5},
+                    {'in_channels' : 64, 'out_channels' : 64, 'kernel_size' : 5}
+                ]
+            ),
+            ResidualBlock(
+                layer_configs = [
+                    {'in_channels' : 64, 'out_channels' : 64, 'kernel_size' : 5},
                     {'in_channels' : 64, 'out_channels' : 64, 'kernel_size' : 5}
                 ]
             ),
@@ -65,28 +107,28 @@ class PPGModel(nn.Module):
             )
         )
 
-        self.prototype_layer_1 = PrototypeLayer(
-            n_prototypes = 10,
-            prototype_len = 20,
+        self.prototype_layer_2 = PrototypeLayer(
+            n_prototypes = 25,
+            prototype_len = 40,
             prototype_channels = 128,
         )
 
-        self.temporal_mixing_layer_1 = nn.Sequential(
+        self.temporal_mixing_layer_2 = nn.Sequential(
             ResidualBlock(
                 layer_configs = [
-                    {'in_channels' : 10, 'out_channels' : 20, 'kernel_size' : 3},
-                    {'in_channels' : 20, 'out_channels' : 20, 'kernel_size' : 3}
+                    {'in_channels' : 25, 'out_channels' : 50, 'kernel_size' : 3},
+                    {'in_channels' : 50, 'out_channels' : 50, 'kernel_size' : 3}
                 ]
             ),
             ResidualBlock(
                 layer_configs = [
-                    {'in_channels' : 20, 'out_channels' : 40, 'kernel_size' : 5},
-                    {'in_channels' : 40, 'out_channels' : 40, 'kernel_size' : 5}
+                    {'in_channels' : 50, 'out_channels' : 50, 'kernel_size' : 5},
+                    {'in_channels' : 100, 'out_channels' : 100, 'kernel_size' : 5}
                 ]
             )
         )
 
-        self.encoder_layer_2 = nn.Sequential(
+        self.encoder_layer_3 = nn.Sequential(
             ResidualBlock(
                 layer_configs = [
                     {'in_channels' : 128, 'out_channels' : 256, 'kernel_size' : 5, 'dilation' : 2},
@@ -101,13 +143,43 @@ class PPGModel(nn.Module):
             )
         )
 
-        self.prototype_layer_2 = PrototypeLayer(
-            n_prototypes = 10,
-            prototype_len = 20,
+        self.prototype_layer_3 = PrototypeLayer(
+            n_prototypes = 25,
+            prototype_len = 40,
             prototype_channels = 512,
         )
 
-        self.temporal_mixing_layer_2 = nn.Sequential(
+        self.temporal_mixing_layer_3 = nn.Sequential(
+            ResidualBlock(
+                layer_configs = [
+                    {'in_channels' : 25, 'out_channels' : 50, 'kernel_size' : 3},
+                    {'in_channels' : 50, 'out_channels' : 50, 'kernel_size' : 3}
+                ]
+            ),
+            ResidualBlock(
+                layer_configs = [
+                    {'in_channels' : 50, 'out_channels' : 50, 'kernel_size' : 5},
+                    {'in_channels' : 100, 'out_channels' : 100, 'kernel_size' : 5}
+                ]
+            )
+        )
+
+        self.encoder_layer_4 = nn.Sequential(
+            ResidualBlock(
+                layer_configs = [
+                    {'in_channels' : 512, 'out_channels' : 512, 'kernel_size' : 5, 'dilation' : 32},
+                    {'in_channels' : 512, 'out_channels' : 512, 'kernel_size' : 5, 'dilation' : 64}
+                ]
+            )
+        )
+
+        self.prototype_layer_4 = PrototypeLayer(
+            n_prototypes = 10,
+            prototype_len = 40,
+            prototype_channels = 512,
+        )
+
+        self.temporal_mixing_layer_4 = nn.Sequential(
             ResidualBlock(
                 layer_configs = [
                     {'in_channels' : 10, 'out_channels' : 20, 'kernel_size' : 3},
@@ -123,14 +195,14 @@ class PPGModel(nn.Module):
         )
 
         self.last_layer = nn.Linear(
-            in_features = 80,
+            in_features = 340,
             out_features = representation_dimension
         )
 
         # model components for stagewise training 
-        self.encoder_layers = [self.encoder_layer_1, self.encoder_layer_2]
-        self.prototype_layers = [self.prototype_layer_1, self.prototype_layer_2]
-        self.temporal_mixing_layers = [self.temporal_mixing_layer_1, self.temporal_mixing_layer_2]
+        self.encoder_layers = [self.encoder_layer_1, self.encoder_layer_2, self.encoder_layer_3, self.encoder_layer_4]
+        self.prototype_layers = [self.prototype_layer_1, self.prototype_layer_2, self.prototype_layer_3, self.prototype_layer_4]
+        self.temporal_mixing_layers = [self.temporal_mixing_layer_1, self.temporal_mixing_layer_2, self.temporal_mixing_layer_3, self.temporal_mixing_layer_4]
         # IMPORTANT: masking_encoder, mask_token, and last_layer are required but accessed directly 
 
     def forward(self, x):
@@ -142,7 +214,7 @@ class PPGModel(nn.Module):
         else: # add trivial mask channel during evaluation for consistent input shape
             zero_mask = torch.zeros(z.size(0), 1, z.size(-1), device = z.device, dtype = z.dtype)
             z = torch.cat([z, zero_mask], dim = 1)
-        
+
         feature_maps = []
         for encoder_layer, prototype_layer, temporal_mixing_layer in zip(self.encoder_layers, self.prototype_layers, self.temporal_mixing_layers):
             z = encoder_layer(z)
